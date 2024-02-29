d3f0ec087d924b81f167e1e32b1d2008c73be59a
ANKUSH KUMAR <77243849+Kankush18@users.noreply.github.com>
2024-02-29 15:09:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+document.getElementById('change_button').addEventListener('click', function() {
+    const blockId = document.getElementById('block_id').value;
+    const color = document.getElementById('colour_id').value;
+    const block = document.getElementById(blockId);
+    
+    if (block) {
+        
+        const gridItems = document.querySelectorAll('.grid-item');
+        gridItems.forEach(item => {
+            item.style.backgroundColor = 'transparent';
+        });
+        
+        
+        block.style.backgroundColor = color;
+    } else {
+        alert('Invalid block ID!');
+    }
+});
+
+document.getElementById('reset_button').addEventListener('click', function() {
+
+    const gridItems = document.querySelectorAll('.grid-item');
+    gridItems.forEach(item => {
+        item.style.backgroundColor = 'transparent';
+    });
+});
